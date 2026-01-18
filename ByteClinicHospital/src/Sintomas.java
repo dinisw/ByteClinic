fa823c0a1a5357b6005829bed1741a7e23d1b10a
1252039 <1252039@isep.ipp.pt>
2026-01-18 20:02:48 +0000

    classe sintomas corregida

--- ByteClinicHospital/src/Sintomas.java
+++ ByteClinicHospital/src/Sintomas.java
@@ -1,12 +1,15 @@
 public class Sintomas {
     private String nomeSintoma;
     private String cor;
-    private String especialidadesAssociadas;
+    private String[] especialidadesAssociadas;
 
     public Sintomas(String nomeSintoma, String cor, String especialidadesAssociadas) {
         this.nomeSintoma = nomeSintoma;
         this.cor = cor;
-        this.especialidadesAssociadas = especialidadesAssociadas;
+        this.especialidadesAssociadas = new String[]{especialidadesAssociadas};
+    }
+
+    public Sintomas(String nome, String cor, String[] especialidades) {
     }
 
     public String getNomeSintoma() {
@@ -25,12 +28,12 @@ public class Sintomas {
         this.cor = cor;
     }
 
-    public String getEspecialidadesAssociadas() {
+    public String[] getEspecialidadesAssociadas() {
         return especialidadesAssociadas;
     }
 
     public void setEspecialidadesAssociadas(String especialidadesAssociadas) {
-        this.especialidadesAssociadas = especialidadesAssociadas;
+        this.especialidadesAssociadas = new String[]{especialidadesAssociadas};
     }
 
     @Override
@@ -40,5 +43,16 @@ public class Sintomas {
                 ", cor='" + cor + '\'' +
                 ", especialidadesAssociadas='" + especialidadesAssociadas + '\'' +
                 '}';
+    }
+
+    public String paraFicheiro() {
+        String linha = nomeSintoma + ";" + cor;
+        if (especialidadesAssociadas != null) {
+            for (int i = 0; i < especialidadesAssociadas.length; i++) {
+                linha += ";" + especialidadesAssociadas[i];
+            }
+        }
+
+        return linha;
     }
 }
